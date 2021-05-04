8b9fd37786c840ca99e2df8215de51b38285df27
Jimmy-sheep <65125836+Jimmy-sheep@users.noreply.github.com>
2021-05-04 23:21:19 +0800

    Update Title.js

--- src/Title.js
+++ src/Title.js
@@ -1,4 +1,4 @@
-const styleArgument = { fontSize:'100px', color:'build' };
+const styleArgument = { fontSize:'100px', color:'blue' };
 
 const Title=()=>{
 	return <h1 style = {styleArgument}> hello World!! </h1>;
